f044754f664b04fb8dc1c62932db666dd78fa206
Jose Carlos Junior <junior.edf@gmail.com>
2020-09-21 21:29:54 -0300

    Títulos, seleção de um item: - Adicionado título às telas; - Implementada seleção automática de um item da lista;

--- NextChoose/NextChoose/Modules/ChooseOptionPage.xaml.cs
+++ NextChoose/NextChoose/Modules/ChooseOptionPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NextChoose.Models;
 using NextChoose.Utils;
 using Xamarin.Forms;
@@ -10,32 +11,58 @@ namespace NextChoose.Modules
     {
         public string SelectedOption { get; set; }
         public List<OptionItem> Options { get; private set; }
-        int optionIndex = 0;
+        int optionIndex;
+
+        int randomEndObj;
+        int count;
 
         public ChooseOptionPage()
         {
             InitializeComponent();
+
+            //Desordanação simples da lista
+            var randomObj = new Random();
+            Options = OptionItemMock.GetOptionItems().OrderBy(x => randomObj.Next()).ToList();
+
+            lblSelectedOption.Text = Options[optionIndex].Title;
 
             BindingContext = this;
         }
 
         private void ChooseOption_Clicked(object sender, EventArgs e)
         {
-            optionIndex = 0;
-            Options = new List<OptionItem>(OptionItemMock.GetOptionItems());
+            count = 0;
+            randomEndObj = new Random().Next(40, 60);
 
-            Device.StartTimer(TimeSpan.FromSeconds(0.2), () => OnTimerTick());
+            optionIndex = 0;
+
+            Device.StartTimer(TimeSpan.FromSeconds(0.05), () => OnTimerTickAsync());
+
+            (sender as Button).IsEnabled = false;
         }
 
-        bool OnTimerTick()
+        bool OnTimerTickAsync()
         {
-
             Device.BeginInvokeOnMainThread(() =>
             {
                 lblSelectedOption.Text = Options[optionIndex].Title;
                 if (++optionIndex >= Options.Count)
                     optionIndex = 0;
             });
+
+            count++;
+            if (count == randomEndObj)
+            {
+
+                lblSelectedOption.ScaleTo(2, 200).ContinueWith((retorno) =>
+                {
+                    lblSelectedOption.ScaleTo(1, 200).Wait();
+                });
+
+                btnChooseOption.IsEnabled = true;
+                return false;
+            }
+
             return true;
         }
     }
